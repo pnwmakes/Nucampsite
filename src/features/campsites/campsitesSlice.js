cf656b09ab18be71fee5d5dd61fa56b0660c1287
Jim <pnwmakes@gmail.com>
2023-03-20 19:56:35 -0700

    campsitesSlice

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,12 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectRandomCampsite = () => {
+    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+};
+
+
+
