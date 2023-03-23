d6f6a0ca676355cce723390985238639918b905b
Jim <pnwmakes@gmail.com>
2023-03-22 21:07:48 -0700

    Comments

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,6 +2,8 @@ import { Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import CommentsList from '../features/comments/CommentsList';
+
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
@@ -11,9 +13,10 @@ const CampsiteDetailPage = () => {
         <Container>
             <Row>
                 <CampsiteDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
             </Row>
         </Container>
-    )
+    );
 };
 
 export default CampsiteDetailPage;
