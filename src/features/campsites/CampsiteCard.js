cd4cd428e0c8756c88d002cdf2bf5ef39e717864
Jim <pnwmakes@gmail.com>
2023-03-22 19:38:01 -0700

    CampsiteDetailPage with useParams

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const CampsiteCard = ({ campsite }) => {
-    const { image, name } = campsite;
+    const { id, image, name } = campsite;
     return (
-        <Card>
-            <CardImg width='100%' src={image} alt={name} />
-            <CardImgOverlay>
-                <CardTitle>{name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg width='100%' src={image} alt={name} />
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     );
 };
 
