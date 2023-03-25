50cff7a1946a3b6676a4d06767ebb9a0fb0272e8
Jim <pnwmakes@gmail.com>
2023-03-25 12:07:21 -0700

    Final week 4

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-// import { Routes, Route } from 'react-router-dom';
-// import ContactPage from './pages/ContactPage';
-// import HomePage from './pages/HomePage';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-// import CampsiteDetailPage from './pages/CampsiteDetailPage';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className='App'>
-//       <Header />
-//       <Routes>
-//         <Route path='/' element={<HomePage />} />
-//         <Route path='contact' element={<ContactPage />} />
-//         <Route path='directory' element={<CampsitesDirectoryPage />} />
-//         <Route
-//           path='directory/:campsiteId'
-//           element={<CampsiteDetailPage />}
-//         />
-//       </Routes>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { Routes, Route } from 'react-router-dom';
+// import { ScrollRestoration } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage'; // Import the AboutPage component
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -46,8 +18,9 @@ function App() {
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-        <Route path='about' element={<AboutPage />} /> // Add a new Route component
+        <Route path='about' element={<AboutPage />} />
       </Routes>
+
       <Footer />
     </div>
   );
